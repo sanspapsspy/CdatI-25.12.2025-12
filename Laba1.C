ae545bb1c552fd31b75dfd17c48898d422866291
sanspapsspy(Устинов Александр) <116632789+sanspapsspy@users.noreply.github.com>
2025-12-22 10:33:14 +0300

    Update Laba1.C

--- Laba1.C
+++ Laba1.C
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+#include <locale.h>
 
 // Вспомогательные функции
 
@@ -10,7 +11,7 @@ bool is_prime(int n) {
     if (n <= 1) return false;
     if (n <= 3) return true;
     if (n % 2 == 0 || n % 3 == 0) return false;
-    
+
     for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) {
             return false;
@@ -61,7 +62,7 @@ void print_array(const char* label, const int* arr, int size) {
         printf("пустой массив\n");
         return;
     }
-    
+
     printf("[");
     for (int i = 0; i < size; i++) {
         printf("%d", arr[i]);
@@ -83,11 +84,11 @@ int* task1(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     // Выделяем память для результата
     int* C = (int*)malloc(maxSize * sizeof(int));
     *resultSize = 0;
-    
+
     // Заполняем массив C
     for (int i = 0; i < sizeA; i++) {
         if (A[i] % 2 != 0) { // нечетный элемент A
@@ -97,7 +98,7 @@ int* task1(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     return C;
 }
 
@@ -105,7 +106,7 @@ int* task1(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
 int* task2(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
     // Временный массив для отслеживания уже добавленных элементов
     bool* added = (bool*)calloc(sizeA, sizeof(bool));
-    
+
     // Определяем максимально возможный размер результата
     int maxSize = 0;
     for (int i = 0; i < sizeA; i++) {
@@ -121,16 +122,16 @@ int* task2(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     // Выделяем память для результата
     int* C = (int*)malloc(maxSize * sizeof(int));
     *resultSize = 0;
-    
+
     // Сбрасываем массив added
     for (int i = 0; i < sizeA; i++) {
         added[i] = false;
     }
-    
+
     // Заполняем массив C
     for (int i = 0; i < sizeA; i++) {
         if (!added[i]) {
@@ -146,7 +147,7 @@ int* task2(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     free(added);
     return C;
 }
@@ -155,7 +156,7 @@ int* task2(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
 int* task3(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
     // Временный массив для отслеживания уже добавленных элементов
     bool* added = (bool*)calloc(sizeA, sizeof(bool));
-    
+
     // Определяем максимально возможный размер результата
     int maxSize = 0;
     for (int i = 0; i < sizeA; i++) {
@@ -182,16 +183,16 @@ int* task3(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     // Выделяем память для результат
     int* C = (int*)malloc(maxSize * sizeof(int));
     *resultSize = 0;
-    
+
     // Сбрасываем массив added
     for (int i = 0; i < sizeA; i++) {
         added[i] = false;
     }
-    
+
     // Заполняем массив C
     for (int i = 0; i < sizeA; i++) {
         if (!added[i]) {
@@ -218,7 +219,7 @@ int* task3(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     free(added);
     return C;
 }
@@ -227,7 +228,7 @@ int* task3(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
 int* task4(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
     // Временный массив для отслеживания уже добавленных элементов
     bool* added = (bool*)calloc(sizeA, sizeof(bool));
-    
+
     // Определяем максимально возможный размер результата
     int maxSize = 0;
     for (int i = 0; i < sizeA; i++) {
@@ -243,16 +244,16 @@ int* task4(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     // Выделяем память для результата
     int* C = (int*)malloc(maxSize * sizeof(int));
     *resultSize = 0;
-    
+
     // Сбрасываем массив added
     for (int i = 0; i < sizeA; i++) {
         added[i] = false;
     }
-    
+
     // Заполняем массив C
     for (int i = 0; i < sizeA; i++) {
         if (!added[i]) {
@@ -268,7 +269,7 @@ int* task4(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     free(added);
     return C;
 }
@@ -277,7 +278,7 @@ int* task4(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
 int* task5(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
     // Временный массив для отслеживания уже добавленных элементов
     bool* added = (bool*)calloc(sizeA, sizeof(bool));
-    
+
     // Определяем максимально возможный размер результата
     int maxSize = 0;
     for (int i = 0; i < sizeA; i++) {
@@ -301,16 +302,16 @@ int* task5(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     // Выделяем память для результата
     int* C = (int*)malloc(maxSize * sizeof(int));
     *resultSize = 0;
-    
+
     // Сбрасываем массив added
     for (int i = 0; i < sizeA; i++) {
         added[i] = false;
     }
-    
+
     // Заполняем массив C
     for (int i = 0; i < sizeA; i++) {
         if (!added[i]) {
@@ -334,7 +335,7 @@ int* task5(const int* A, int sizeA, const int* B, int sizeB, int* resultSize) {
             }
         }
     }
-    
+
     free(added);
     return C;
 }
@@ -352,17 +353,18 @@ int task6(const int* A, int sizeA) {
 // Основная функция для демонстрации работы
 int main() {
     // Тестовые данные
-    int A[] = {2, 3, 5, 7, 3, 8, 10, 5, 13, 17, 19, 4, 6, 8, -5, -3};
-    int B[] = {3, 5, 8, 10, 12, 14, 16, 3, 5, 7, -3, -5};
-    
+    setlocale(LC_ALL, ("Rus"));
+    int A[] = { 2, 3, 5, 7, 3, 8, 10, 5, 13, 17, 19, 4, 6, 8, -5, -3 };
+    int B[] = { 3, 5, 8, 10, 12, 14, 16, 3, 5, 7, -3, -5 };
+
     int sizeA = sizeof(A) / sizeof(A[0]);
     int sizeB = sizeof(B) / sizeof(B[0]);
-    
+
     printf("Исходные массивы:\n");
     print_array("A", A, sizeA);
     print_array("B", B, sizeB);
     printf("\n");
-    
+
     // Задание 1
     printf("ЗАДАНИЕ 1:\n");
     printf("Найти нечетные элементы А, которых нет в В\n");
@@ -376,7 +378,7 @@ int main() {
     print_array("C", C1, sizeC1);
     free(C1);
     printf("\n");
-    
+
     // Задание 2
     printf("ЗАДАНИЕ 2:\n");
     printf("Найти элементы А, которые есть в В более, чем в одном экземпляре\n");
@@ -390,7 +392,7 @@ int main() {
     print_array("C", C2, sizeC2);
     free(C2);
     printf("\n");
-    
+
     // Задание 3
     printf("ЗАДАНИЕ 3:\n");
     printf("Найти неповторяющиеся элементы А, которых нет в В\n");
@@ -404,7 +406,7 @@ int main() {
     print_array("C", C3, sizeC3);
     free(C3);
     printf("\n");
-    
+
     // Задание 4
     printf("ЗАДАНИЕ 4:\n");
     printf("Найти четные элементы А, которые есть в В\n");
@@ -418,7 +420,7 @@ int main() {
     print_array("C", C4, sizeC4);
     free(C4);
     printf("\n");
-    
+
     // Задание 5
     printf("ЗАДАНИЕ 5:\n");
     printf("Найти элементы А по модулю равные некоторым элементам В\n");
@@ -442,7 +444,7 @@ int main() {
     print_array("C", C5, sizeC5);
     free(C5);
     printf("\n");
-    
+
     // Задание 6
     printf("ЗАДАНИЕ 6:\n");
     printf("Найти последний по порядку элемент, являющийся простым числом\n");
@@ -452,22 +454,24 @@ int main() {
     int last_prime = task6(A, sizeA);
     if (last_prime != -1) {
         printf("Результат: %d\n", last_prime);
-    } else {
+    }
+    else {
         printf("В массиве A нет простых чисел\n");
     }
-    
+
     // Дополнительный пример для задачи 6
     printf("\nДополнительный пример для задачи 6:\n");
-    int testArray[] = {4, 6, 8, 9, 10, 12, 14, 15};
+    int testArray[] = { 4, 6, 8, 9, 10, 12, 14, 15 };
     int testSize = sizeof(testArray) / sizeof(testArray[0]);
     print_array("Массив", testArray, testSize);
     printf("Ожидаемый результат: нет простых чисел\n");
     int result = task6(testArray, testSize);
     if (result != -1) {
         printf("Последний простой элемент: %d\n", result);
-    } else {
+    }
+    else {
         printf("В массиве нет простых чисел (ожидаемо для этого массива)\n");
     }
-    
+
     return 0;
 }
